93a1ea87777db4069952f7801e60587b69505016
JadedLeader <JY263@student.aru.ac.uk>
2022-10-06 14:31:44 +0100

    do while, try catch, calculator

--- Program.cs
+++ Program.cs
@@ -1,4 +1,109 @@
-﻿int user_input1, user_input2, user_input3, user_input4, user_input5, user_input6;
+﻿int num1;
+string method;
+int num2;
+int addition;
+int subtraction;
+int mod;
+int division;
+int multi;
+char choice;
+
+do
+{
+    try
+    {
+        Console.WriteLine("Please input number one");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Please input the operator you want to use: ");
+        method = Console.ReadLine();
+
+        Console.WriteLine("Please input number two: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        switch (method)
+        {
+            case "+":
+                Console.WriteLine("You have chosen the addition operator");
+                addition = num1 + num2;
+                Console.WriteLine("This is the end result of your addition: " + addition);
+                break;
+
+            case "-":
+                Console.WriteLine("You have chosen the subtraction operator");
+                subtraction = num1 - num2;
+                Console.WriteLine("This is the end result of your subtraction: " + subtraction);
+                break;
+
+            case "*":
+                Console.WriteLine("You have chosen the multiplication operator");
+                multi = num1 * num2;
+                Console.WriteLine("This is the end result of your multiplication: " + multi);
+                break;
+
+            case "/":
+                Console.WriteLine("You have chosen the division operator");
+                division = num1 / num2;
+                Console.WriteLine("This is the end result of your division: " + division);
+                break;
+
+            case "%":
+                Console.WriteLine("You have chosen the mod operator");
+                mod = num1 % num2;
+                Console.WriteLine("This is the end result of your mod " + mod);
+                break;
+
+
+        }
+        
+    }
+    catch
+    { 
+        Console.WriteLine("Invalid");
+    }
+    Console.WriteLine("Do you want to continue? ");
+    choice = Convert.ToChar(Console.ReadLine());
+}
+while (choice != 'N');
+Console.ReadKey();
+
+/* random number 
+random rand = new random();
+int number = rand.next(0, 100); */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*int user_input1, user_input2, user_input3, user_input4, user_input5, user_input6;
 int average;
 char choice;
 
@@ -63,7 +168,7 @@ do
 
 }
 while(choice != 'N');
-Console.ReadKey();
+Console.ReadKey(); */
 
 
 
